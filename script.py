1a28191c474bd8e689a4d8a13b3857deb21e076a
HAV0X underscore <73144620+HAV0X1014@users.noreply.github.com>
2023-11-07 18:18:02 -0500

    Change to DuckDuckGo as search provider

--- script.py
+++ script.py
@@ -1,34 +1,22 @@
 import gradio as gr
 import modules.shared as shared
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
+from duckduckgo_search import DDGS
+
 import urllib
 
-search_access = False
-service = Service()
-options = webdriver.ChromeOptions()
-options.add_argument('headless')
-options.add_argument('--disable-infobars')
-options.add_argument('--disable-dev-shm-usage')
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-gpu')
-options.add_argument('--remote-debugging-port=9222')
+search_access = True
 
-def google_results(query):
-    
-    driver = webdriver.Chrome(service=service,options=options)
-    query = urllib.parse.quote_plus(query)
-    url="https://www.google.com/search?hl=en&q="+query
-    driver.get(url)
-    html = driver.find_element(By.CLASS_NAME, 'ULSxyf').text
-    driver.quit()
+def search_results(query):
+    html = ""
+    with DDGS() as ddgs:
+        for r in ddgs.text(query, safesearch='off', timelimit='y', max_results=10):
+            html = html + str(r) + "\n"
     return html
 
 
 def ui():
     global search_access
-    checkbox = gr.Checkbox(value=search_access, label="Enable Google Search")
+    checkbox = gr.Checkbox(value=search_access, label="Enable DuckDuckGo Search")
     checkbox.change(fn=update_search_access, inputs=checkbox)
     return checkbox, search_access
 
@@ -45,11 +33,9 @@ def input_modifier(user_input, state):
         if user_input.lower().startswith("search"):
             shared.processing_message = "*Searching online...*"
             query = user_input.replace("search", "").strip()
-            state[
-                    "context"
-                ] = "The answer to User question is provided to you in Google search results. Give a truthful and correct answer. Answer the question"
-            search_data = google_results(query) 
-            user_prompt = f"User question: {user_input}\n Google search results: {search_data}"
+            state["context"] = state["context"] + "Relevant search results are in the DuckDuckGo search results. Use this info in the response."
+            search_data = search_results(query)
+            user_prompt = f"User question: {user_input}\nDuckDuckGo search results: {search_data}"
             return str(user_prompt)               
     shared.processing_message = "*Typing...*"
     return user_input
